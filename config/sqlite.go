533f15c9d81a3452e94733323b0d74195c88581f
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-01-08 15:19:12 -0300

    err: ERROR to create DB

--- config/sqlite.go
+++ config/sqlite.go
@@ -8,34 +8,34 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitializeSQlite() (*gorm.DB, error) {
+func InitializeSQLite() (*gorm.DB, error) {
 	logger := GetLogger("sqlite")
-	dbpath := "./db/main.db"
-	_, err := os.Stat(dbpath)
+	dbPath := "./db/main.db"
+	_, err := os.Stat(dbPath)
 
 	if os.IsNotExist(err) {
 		logger.Info("database file not found, creating...")
 		err = os.MkdirAll("./db", os.ModePerm)
+
 		if err != nil {
 			return nil, err
 		}
-		file, err := os.Create(dbpath)
+
+		file, err := os.Create(dbPath)
 		if err != nil {
 			return nil, err
 		}
 		file.Close()
 	}
 
-	db, err := gorm.Open(sqlite.Open(dbpath), &gorm.Config{})
-
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if err != nil {
-		logger.Errorf("Sqlite opening error: %v", err)
+		logger.Errorf("sqlite opening error: %v", err)
 		return nil, err
 	}
-
 	err = db.AutoMigrate(&schemas.Opening{})
 	if err != nil {
-		logger.Errorf("Sqlite automigration error: %v", err)
+		logger.Errorf("sqlite automigration error: %v", err)
 		return nil, err
 	}
 
